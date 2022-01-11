b77ab0e61b66c23751267d5e782c14abbef98ba1
alexandra <alexandraschurman@gmail.com>
2022-01-11 09:11:21 -0800

    commit tree 575e9ac4dd0a575baad150d8103bb120c03f8e2f

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+
+
+
+
+
+/*
+// function saturdayFun(activity = "roller-skate") {
+//     return `This Saturday, I want to ${activity}!`
+// }
+
+// function mondayWork(mondayActivity = "go to the office") {
+//    return `This Monday, I will ${mondayActivity}.`
+// }
+
+mondayWork("work from home");
+
+// const mondayWork = function(){
+//     let mondayActivity = "go to the office";
+//     return `This Monday, I will ${mondayActivity}.`
+// }
+
+// function wrapAdjective(){
+//     const this = "special";
+//         return function (){
+//             const that = "*";
+//         return function (){
+//             console.log(`You are ${that}${this}${that}!`);
+//         }
+// }
+// }
+
+
+
+// const encouragingPromtFunction = wrapAdjective(this = "special"){
+//     return function () {
+//         console.log(`You are`)
+// }
+
+// }
+*/
